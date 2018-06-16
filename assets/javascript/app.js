c49efbe0279fe8ffb22f9e4ab9eb5fb9e469aa41
Doogle <fdunigan11@gmail.com>
2018-06-16 12:26:17 -0400

    second changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,8 @@ function checker() {
     var question1 = document.quiz.question1.value;
     var question2 = document.quiz.question2.value;
     var question3 = document.quiz.question3.value;
+    var question4 = document.quiz.question4.value;
+    var question5 = document.quiz.question5.value;
     var correct = 0;
 
     if (question1 == "Gregor Clegane") {
@@ -13,6 +15,14 @@ function checker() {
     }
 
     if (question3 == "Run") {
+        correct++;
+    }
+
+    if (question4 == "Harry S. Truman") {
+        correct++;
+    }
+
+    if (question5 == "Kareem Abdul-Jabbar") {
         correct++;
     }
 
