0bddfd2eb6eac5a44f1ab7998440097ee7b9f58a
Doogle <fdunigan11@gmail.com>
2018-06-16 16:17:27 -0400

    final changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,24 @@
+var i = 30;
+
+function timer() {
+    var x = document.getElementById("timer").innerHTML = i;
+    i--;
+}
+
+var m = setInterval("timer()", 1000);
+
+function xx() {
+    clearInterval(m);
+}
+
+setTimeout("xx()", 31000);
+
 function checker() {
     var question1 = document.quiz.question1.value;
     var question2 = document.quiz.question2.value;
     var question3 = document.quiz.question3.value;
     var question4 = document.quiz.question4.value;
-    var question5 = document.quiz.question5.value;
+    var question5 = document.quiz.question5.value;  
     var correct = 0;
 
     if (question1 == "Gregor Clegane") {
